8fa6990fc4dba46f91b06470f2e83ae8d960ebad
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2025-12-16 19:20:14 +0530

    Time: 1 ms (99.93%), Space: 42.6 MB (63.24%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long sumAndMultiply(int n) {
+        int x = 0, sum = 0, i = 1;
+        while(n > 0) {
+            int m = n % 10;
+            n /= 10;
+            if(m == 0) continue;
+            sum += m;
+            x += (m * i);
+            i *= 10;
+        }
+        return (long) x * sum;
+    }
+}
