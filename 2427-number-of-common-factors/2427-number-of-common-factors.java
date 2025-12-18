f425fa9ccf460b66265379ae397a64a8b9cd4b99
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2025-12-18 06:32:15 +0530

    Time: 1 ms (58.84%), Space: 41.9 MB (57.89%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int count=0;
+        if(a==b){
+            for(int i=1;i<=a;i++){
+            if(a%i==0){
+                count++;
+            }
+        }
+        return count;
+        }
+        for(int i=1;i<=a;i++){
+            if(a%i==0 && b%i==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
