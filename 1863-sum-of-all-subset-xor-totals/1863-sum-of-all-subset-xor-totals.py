077ecb79e8cad1617eb1826e4d443ebeb26b5709
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2025-12-19 07:48:27 +0530

    Time: 0 ms (100%), Space: 17.3 MB (100%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        or_all = 0
+        for x in nums:
+            or_all |= x
+        return or_all * (1 << (len(nums) - 1))
