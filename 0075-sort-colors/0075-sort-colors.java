6121824b6e6f4da5c33cbdbdbb251fd005fa1bf6
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2026-01-13 06:54:08 +0530

    Time: 0 ms (100%), Space: 43.7 MB (14.43%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int c0=0;
+        int c1=0;
+        int c2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                c1++;
+            }else if(nums[i]==0){
+                c0++;
+            }else{
+                c2++;
+            }
+        }
+        for(int i=0;i<c0;i++) nums[i]=0;
+        for(int i=c0;i<c0+c1;i++) nums[i]=1;
+        for(int i=c1+c0;i<c2+c1+c0;i++) nums[i]=2;
+
+        
+    }
+}
