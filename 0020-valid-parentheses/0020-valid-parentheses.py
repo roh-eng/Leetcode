55f737f742b9016514c1f7251530488f781e7fc3
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2026-02-10 19:13:51 +0530

    Time: 0 ms (100%), Space: 19.5 MB (17.99%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        stack = []
+        pairs = {'(': ')', '{': '}', '[': ']'}
+        
+        for char in s:
+            if char in pairs:  
+                stack.append(char)
+            else:  
+                if not stack or pairs[stack.pop()] != char:
+                    return False
+    
+        return len(stack) == 0
+            
