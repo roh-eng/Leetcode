6f4c12f38e4e41cbf41ba170d4e335ba9d677b14
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2026-02-13 05:39:28 +0530

    Time: 2 ms (3.69%), Space: 46.5 MB (10.57%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -2,35 +2,10 @@ import java.util.*;
 
 class Solution {
     public boolean search(int[] nums, int k) {
-        int low = 0;
-        int high = nums.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (nums[mid] == k) return true;
-
-            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
-                low++;
-                high--;
-                continue;
-            }
-
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= k && k <= nums[mid]) {
-                    high = mid - 1; 
-                } else {
-                    low = mid + 1;  
-                }
-            }
-            else {
-                if (nums[mid] <= k && k <= nums[high]) {
-                    low = mid + 1;  
-                } else {
-                    high = mid - 1; 
-                }
-            }
+        Set<Integer> set1 = new HashSet<>();
+        for(int num : nums) {
+            set1.add(num);
         }
-
-        return false; 
+        return set1.contains(k);
     }
 }
