12664acd941ad4310b064ced20d294dbe6176945
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2026-03-01 05:41:23 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.24%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        x=int(num1)
+        y=int(num2)
+        return str(x*y)
