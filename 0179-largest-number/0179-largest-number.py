3b0afd71e63211b2ccf8405b93a0d7116279328d
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2026-03-04 05:49:17 +0530

    Time: 3 ms (58.3%), Space: 19.3 MB (84.66%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,19 @@
+from functools import cmp_to_key
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = list(map(str, nums))
+        
+        def compare(x, y):
+            if x + y > y + x:
+                return -1
+            elif x + y < y + x:
+                return 1
+            else:
+                return 0
+        
+        nums.sort(key=cmp_to_key(compare))
+        
+        result = ''.join(nums)
+        
+        # Handle case like [0,0]
+        return '0' if result[0] == '0' else result
