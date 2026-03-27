86c01f5eb98ce47bb994b53a374082143232d321
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2026-03-27 10:34:55 +0530

    Time: 740 ms (91.95%), Space: 0B (100%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,7 @@
+SELECT 
+    Users.name, 
+COALESCE(SUM(Rides.distance), 0) AS travelled_distance
+FROM Users
+LEFT JOIN Rides ON Users.id = Rides.user_id
+GROUP BY Users.id, Users.name
+ORDER BY travelled_distance DESC, Users.name ASC;
