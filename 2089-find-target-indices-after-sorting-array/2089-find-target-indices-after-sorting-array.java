321a5c2338b2d8b75f88464b7672cb99a6a8b2e0
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2026-03-28 11:30:59 +0530

    Time: 6 ms (28.32%), Space: 50.7 MB (7.07%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        Arrays.sort(nums);
+        ArrayList<Integer> res=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+}
