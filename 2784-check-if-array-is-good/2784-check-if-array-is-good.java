f60f5d86d732d656d984cdb3d8d624440a4b39ee
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2026-03-30 19:19:58 +0530

    Time: 5 ms (45.94%), Space: 45.1 MB (38.34%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+        
+        int n = nums.length - 1;
+     
+        if (n < 1) return false;
+
+ 
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return false;
+            }
+        }
+   
+        return nums[n] == n;
+    }
+}
