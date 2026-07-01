81aad42b4d1e59ba2eaf4c51f451ebaf00c3bf09
DEVADULA ROHIT <rohitdevadula7@gmail.com>
2026-07-01 19:41:52 +0530

    Time: 0 ms (100%), Space: 42.1 MB (33.1%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        // if( n == 0 || n==1){
+        //     return n;
+        // }
+        // if(n%2!=0){
+        //     return n/3;
+        // }
+        return (int) (Math.sqrt(n));
+    }
+}
