e95a251bac0595c480f300e0c10c90073ccfedc9
DEVADULA ROHIT <rohitdevadula7@gmail.com>
2026-09-12 13:19:17 +0530

    Time: 2 ms (85.84%), Space: 44.1 MB (86.14%) - LeetHub

---
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    for(int k=j+1;k<nums.length;k++){
+                        if(nums[j]==nums[k]){
+                            
+                            ans=Math.min(2*(k-i),ans);
+                        }
+                    }
+                }
+            }
+        }
+        // return ans;
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+
+    }
+}
