135f5b9d4ec0cc3104823b03b362935524c93e4f
DEVADULA ROHIT <rohitdevadula7@gmail.com>
2026-09-24 10:23:46 +0530

    Time: 1 ms (99.69%), Space: 45.8 MB (24.38%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,19 +1,22 @@
 class Solution {
     public int smallestIndex(int[] nums) {
+        //int ans=0;
+        //int sum=0;
         int last=0;
-        
         for(int i=0;i<nums.length;i++){
             int sum=0;
-            while(nums[i]>0){
-                last=nums[i]%10;
-                sum=sum+last;
-                nums[i]=nums[i]/10;
-            }
-            if(sum==i){
-                return i;
-            }
+            int num=nums[i];
             
-        }
+            while(num>0){
+                last=num%10;
+                sum=sum+last;
+                num=num/10;
+            }
+                if(sum==i){
+                    return i;
+                }
+            }
+        
         return -1;
     }
 }
